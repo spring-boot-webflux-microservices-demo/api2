b4201741a1d6f2631ec33ef0c85391aacac9fbff
Viktoras Lipinskis <viktoras.lipinskis@gmail.com>
2018-07-01 09:05:36 +0300

    implemented controller with pojo class

--- src/main/java/com/vk/demo/api2/controllers/HelloController.java
+++ src/main/java/com/vk/demo/api2/controllers/HelloController.java
@@ -1,15 +1,23 @@
 package com.vk.demo.api2.controllers;
 
+import com.vk.demo.api2.model.Pojo2;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api2")
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello from api2";
+    public Flux<Pojo2> hello() {
+        return Flux.just(
+                new Pojo2("1", "api2 awesome"),
+                new Pojo2("2", "api2 it"),
+                new Pojo2("3", "api2 works!")
+        );
     }
 }
